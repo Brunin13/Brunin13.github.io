86176730b86e05ae3804c5d47a5bf314fbb8b7ed
Brunin13 <106404730+Brunin13@users.noreply.github.com>
2022-06-04 20:21:08 -0300

    Add files via upload

--- html/Coin Clicker/js/script.js
+++ html/Coin Clicker/js/script.js
@@ -1,8 +1,27 @@
-var subir = clicks,
-phant;
-var coin = 0,
-clicks = 0,
-mult = 1,
+
+var storage1 = parseInt(localStorage.getItem('coinsValue'));
+if (storage1 == '' || storage1 == null) {
+    storage1 = 0;
+}
+
+var storage2 = parseInt(localStorage.getItem('clicksValue'));
+if (storage2 == '' || storage2 == null) {
+    storage2 = 0;
+}
+
+var storage3 = parseInt(localStorage.getItem('multValue'));
+if (storage3 == '' || storage3 == null) {
+    storage3 = 1;
+}
+
+var coin = storage1,
+clicks = storage2,
+mult = storage3,
+subir = 0,
+phant,
+Pcoin,
+Pclicks,
+Pmult,
 isFrist = false, isFrist2 = false, isFrist3 = true;
 
 // variaveis
@@ -16,7 +35,8 @@ DisCPC = document.getElementById("display7"),
 AutoClick1 = 0,
 AutoClick2 = 0,
 pct = document.getElementById("porcentagem");
-
+pct.style.width = "0%";
+var conter= 0;
 
 var coinElement = document.getElementById("coin");
 var is1 = true, is2 = true, cnt = 0;
@@ -25,7 +45,13 @@ var audio = document.getElementById('audio');
 
 //eventos
 
-window.addEventListener('load', function load () {
+var temp = setInterval(() => {
+    localStorage.setItem('coinsValue', coin);
+    localStorage.setItem('clicksValue', clicks);
+    localStorage.setItem('multValue', mult);
+}, 500);
+
+window.addEventListener('load', function load() {
     var mostar = localStorage.getItem('mostrar');
     if (mostar == "S" || mostar == "" || mostar == null) {
         alert("Bem-vindo ao Coin Clicker, este é o seu tutorial");
@@ -58,19 +84,27 @@ function atualizar() {
 
 atualizar();
 
+function goRed() {
+    pct.classList.add("goRed");
+    var time = setInterval(() => {
+        pct.classList.remove("goRed");
+        clearInterval(time);
+    }, 5000);
+}
+
 function raizer() {
     isFrist3 = false;                
-    phant = clicks;
     setInterval(() => {
-        subir = clicks - phant;
-        if (subir >= 10) {
-            pct.classList.add("goRed");
-            var time = setInterval(() => {
-                pct.classList.remove("goRed");
-                clearInterval(time);
-            }, 5000);
+        var tespos = setInterval(() => {
+            clearInterval(tespos);
+        }, 200);
+        if (subir >= 1) {
+            subir += clicks
+            pct.style = "width: " + clicks + "%;";
+            subir -= clicks - 3;
         }
-    }, 500);
+        
+    }, 800);
 }
 
 
@@ -93,7 +127,7 @@ Title.addEventListener('click', function titulo() {
     var intervalo = setTimeout(() => {
             Title.classList.remove('grow');
             clearInterval(intervalo);
-    }, 1000);
+    }, 1900);
     if (cnt == 10) {
         Title.innerHTML = "Coin Flaper 2D";
     } else if (cnt == 20) {
@@ -164,4 +198,48 @@ function exit() {
     }else {
         alert("sem clicks");
     }
+}
+
+function salvar() {
+    Pclicks = clicks;
+    Pcoin = coin;
+    Pmult = mult;
+}
+
+function reset() {
+    clicks = 0;
+    coin = 0;
+    mult = 0;
+    localStorage.setItem('coinsValue', 0);
+    localStorage.setItem('clicksValue', 0);
+    localStorage.setItem('multValue', 0);
+    localStorage.setItem('mostrar', 'S');
+    atualizar();
+    mostar = localStorage.getItem('mostrar');
+    if (mostar == "S" || mostar == "" || mostar == null) {
+        alert("Bem-vindo ao Coin Clicker, este é o seu tutorial");
+        alert("clique na moeda giratoria e ganhe coins!");
+        alert("faça upgrades e ganhe muitos coins");
+        while (true) {
+            var mostrar = prompt("mostrar tutorial outra vez? (S/N)");
+            if (mostrar == "N") {
+                alert("okay, isso siginifica que você entendeu :)");
+                localStorage.setItem('mostrar', 'N');
+                break;
+            } else if (mostrar == "S") {
+                alert("entendido!");
+                localStorage.setItem('mostrar', 'S');
+                break;
+            } else {
+                alert("o digito " + mostrar + " é invalido digite (S para Sim e N para Não)");
+            }
+        }
+    }
+}
+
+function carregar() {
+    coin = Pcoin;
+    clicks = Pclicks;
+    Pmult = mult;
+    atualizar();
 }
